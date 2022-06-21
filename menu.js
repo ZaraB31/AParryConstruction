690f3df85c614fea29de9bb39438e4a03b2aa0dd
Zara Bostock <bostock.zara@gmail.com>
2022-06-21 15:44:41 +0100

    banner and services page made responsive

--- menu.js
+++ menu.js
@@ -1,8 +1,11 @@
 function openMenu() {
     var x = document.getElementById("myTopNav");
+    var y = document.getElementById("title");
     if (x.className === "topnav") {
         x.className += " responsive";
+        y.className += " responsive";
     } else {
         x.className = "topnav";
+        y.className = "title";
     }
 }
